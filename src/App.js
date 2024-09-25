89107d05cc7d8814ca61b604d001cf5b1a8a4796
ShilpaNarayan28 <shilpanarayan162@gmai.com>
2024-09-25 11:58:01 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Shopping", completed: false },
+    { id: 2, name: "Cooking", completed: false },
+    { id: 3, name: "Watching movies", completed: false },
+  ]);
+
+  const addTask = (taskName) => {
+    const newTask = {
+      id: tasks.length + 1,
+      name: taskName,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleTaskChange = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Main tasks={tasks} handleTaskChange={handleTaskChange} />
+      <Footer addTask={addTask} tasks={tasks} setTasks={setTasks} />
+    </div>
+  );
+}
+
+export default App;
